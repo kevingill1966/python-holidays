5e2323cce21e0d1312b3bb38ca91ca690cbd94bd
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2023-02-07 12:50:02 +0700

    IE: Update testcase in general and 2023's, add St. Brigid's Day testcase

--- test/countries/test_ireland.py
+++ test/countries/test_ireland.py
@@ -9,29 +9,64 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
-import unittest
 from datetime import date
 from datetime import timedelta as td
 
-import holidays
+from dateutil.relativedelta import MO
+from dateutil.relativedelta import relativedelta as rd
+
+from holidays.constants import FRI
+from holidays.countries.ireland import IE, IRL, Ireland
+
+from test.common import TestCase
 
 
-class TestIreland(unittest.TestCase):
+class TestIreland(TestCase):
     def setUp(self):
-        self.holidays = holidays.Ireland()
+        self.holidays = Ireland()
+        self.holidays_no_observed = Ireland(observed=False)
+
+    def test_country_aliases(self):
+        self.assertCountryAliases(Ireland, IE, IRL)
 
     def test_2020(self):
-        self.assertIn("2020-01-01", self.holidays)  # New Year's Day
-        self.assertIn("2020-03-17", self.holidays)  # St. Patrick's Day
-        self.assertIn("2020-04-13", self.holidays)  # Easter Monday
-        self.assertIn("2020-05-04", self.holidays)  # May Day in IE
-        self.assertNotIn("2020-05-08", self.holidays)  # May Day in UK not IE
-        self.assertIn("2020-06-01", self.holidays)  # June Bank Holiday
-        self.assertIn("2020-08-03", self.holidays)  # Summer Bank Holiday
-        self.assertIn("2020-10-26", self.holidays)  # October Bank Holiday
-        self.assertIn("2020-12-25", self.holidays)  # Christmas Day
-        self.assertIn("2020-12-26", self.holidays)  # Boxing Day
-        self.assertIn("2020-12-28", self.holidays)  # Boxing Day (Observed)
+        self.assertHoliday("2020-01-01", "New Year's Day")
+        self.assertHoliday("2020-03-17", "St. Patrick's Day")
+        self.assertHoliday("2020-04-13", "Easter Monday")
+        self.assertHoliday("2020-05-04", "May Day")
+        self.assertNoHoliday("2020-05-08", "May Day")
+        self.assertHoliday("2020-06-01", "June Bank Holiday")
+        self.assertHoliday("2020-08-03", "August Bank Holiday")
+        self.assertHoliday("2020-10-26", "October Bank Holiday")
+        self.assertHoliday("2020-12-25", "Christmas Day")
+        self.assertHoliday("2020-12-26", "St. Stephen's Day")
+        self.assertHoliday("2020-12-28", "St. Stephen's Day (Observed)")
+
+    def test_2023(self):
+        self.assertHoliday("2023-01-01", "New Year's Day")
+        self.assertHoliday("2023-02-06", "St. Brigid's Day")
+        self.assertHoliday("2023-03-17", "St. Patrick's Day")
+        self.assertHoliday("2023-04-10", "Easter Monday")
+        self.assertHoliday("2023-05-01", "May Day")
+        self.assertHoliday("2023-06-05", "June Bank Holiday")
+        self.assertHoliday("2023-08-07", "August Bank Holiday")
+        self.assertHoliday("2023-10-30", "October Bank Holiday")
+        self.assertHoliday("2023-12-25", "Christmas Day")
+        self.assertHoliday("2023-12-26", "St. Stephen's Day")
+
+    def test_st_brigids_day(self):
+        # St. Brigid's Day
+        self.holidays.observed = False
+
+        for year in range(2023, 2100):
+            dt = date(year, 2, 1)
+
+            if dt.weekday() == FRI:
+                self.assertHoliday(dt, self.holidays)
+            else:
+                self.assertNoHoliday(dt, self.holidays)
+                self.assertHoliday(dt + rd(weekday=MO), self.holidays)
+        self.assertNoHolidayName("St. Brigid's Day", Ireland(years=2022))
 
     def test_may_day(self):
         # Specific Ireland "May Day"
@@ -47,9 +82,9 @@ class TestIreland(unittest.TestCase):
             date(2019, 5, 6),
             date(2020, 5, 4),
         ]:
-            self.assertIn(dt, self.holidays)
-            self.assertNotIn(dt + td(days=-1), self.holidays)
-            self.assertNotIn(dt + td(days=+1), self.holidays)
+            self.assertHoliday(dt, self.holidays)
+            self.assertNoHoliday(dt + td(days=-1), self.holidays)
+            self.assertNoHoliday(dt + td(days=+1), self.holidays)
 
     def test_st_stephens_day(self):
         # St. Stephen's Day
@@ -57,13 +92,13 @@ class TestIreland(unittest.TestCase):
 
         for year in range(1900, 2100):
             dt = date(year, 12, 26)
-            self.assertIn(dt, self.holidays)
-            self.assertNotIn(dt + td(days=+1), self.holidays)
-        self.assertNotIn(date(2009, 12, 28), self.holidays)
-        self.assertNotIn(date(2010, 12, 27), self.holidays)
+            self.assertHoliday(dt, self.holidays)
+            self.assertNoHoliday(dt + td(days=+1), self.holidays)
+        self.assertNoHoliday(date(2009, 12, 28), self.holidays)
+        self.assertNoHoliday(date(2010, 12, 27), self.holidays)
         self.holidays.observed = True
-        self.assertIn(date(2004, 12, 28), self.holidays)
-        self.assertIn(date(2010, 12, 28), self.holidays)
+        self.assertHoliday(date(2004, 12, 28), self.holidays)
+        self.assertHoliday(date(2010, 12, 28), self.holidays)
         for year, day in enumerate(
             [
                 26,
@@ -91,8 +126,8 @@ class TestIreland(unittest.TestCase):
             2001,
         ):
             dt = date(year, 12, day)
-            self.assertIn(dt, self.holidays, dt)
-            self.assertIn(
+            self.assertHoliday(dt, self.holidays, dt)
+            self.assertHoliday(
                 self.holidays[dt],
                 [
                     "St. Stephen's Day",
@@ -103,4 +138,4 @@ class TestIreland(unittest.TestCase):
             )
 
     def test_special_holidays(self):
-        self.assertIn("2022-03-18", self.holidays)
+        self.assertHoliday("2022-03-18", self.holidays)
